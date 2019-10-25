f725c15c15c9045c597f97770c421cef0fc14716
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2019-10-25 09:50:27 +0200

    Class 6. Solved exercicse 3 (If/Else).

--- clase-6/ejercicios/ejercicio_3.js
+++ clase-6/ejercicios/ejercicio_3.js
@@ -14,3 +14,43 @@
 // Usando Switch
 // 	// Solución con switch
 
+
+function process(num1, num2, num3) {
+    if ((isNaN(num1) || num1 === "undefined" || num1 === null || num1 === "") &&
+        (isNaN(num2) || num2 === "undefined" || num2 === null || num2 === "") &&
+        (isNaN(num3) || num3 === "undefined" || num3 === null || num3 === "")) {
+        console.info("Son numeros");
+
+        alert("Inserta un numero");
+    } else {
+        num1 = Number(num1);
+        num2 = Number(num2);
+        num3 = Number(num3);
+
+        // - Resuelto con if/else if
+        if (num1 < 0) {
+            var sumNumbersOperation = num2 + "+" + num3 + "=",
+                sumNumbersResult = (num2 + num3);
+            alert("El primer numero es negativo: " + num1);
+            alert("La suma del numero 2 y 3 es: " + sumNumbersOperation + sumNumbersResult);
+        } else {
+            var productNumbersOperation = num1 + "*" + num2 + "*" + num3 + "=",
+                productsNumberResult = (num1 * num2 * num3);
+            alert("El primer numero no es negativo: " + num1);
+            alert("El producto  2 y 3 es: " + productNumbersOperation + productsNumberResult);
+        }
+
+        // - Resuelto con Switch
+
+
+        // - Resulto con el operador ternario
+    }
+}
+
+
+alert("Inserta 3 numeros:");
+var num1 = prompt("Numero 1:");
+var num2 = prompt("Numero 2:");
+var num3 = prompt("Numero 3:");
+
+process(num1, num2, num3);
