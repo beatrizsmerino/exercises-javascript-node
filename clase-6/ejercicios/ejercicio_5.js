e7bdc23e7503abe0512fd86f20472abffc538321
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2019-10-25 10:53:01 +0200

    Class 6. Exercise 5 solved (If/Else).

--- clase-6/ejercicios/ejercicio_5.js
+++ clase-6/ejercicios/ejercicio_5.js
@@ -1,8 +1,30 @@
 // 5 - Diseña un algoritmo que al introducir un numero nos diga si es positivo o negativo.
 
 // Usando if...else
-// 	// Solución con if...else 
+// 	// Solución con if...else
 // Usando Operador Ternario (?:)
 // 	// Solución con operador ternario
 // Usando Switch
 // 	// Solución con switch
+
+
+
+function numberPositiveNegative(num1) {
+    if (isNaN(num1) || num1 === "undefined" || num1 === null || num1 === "") {
+        alert("Inserta un numero");
+    } else {
+
+        // - Resuelto con if/else if
+        if (num1 < 0) {
+            alert("Tu numero es negativo: " + num1);
+        } else {
+            alert("Tu numero es positivo " + num1);
+        }
+
+    }
+}
+
+alert("Inserta un numero para saber si es positivo o negativo: ");
+var num1 = prompt("Numero: ");
+
+numberPositiveNegative(num1);
