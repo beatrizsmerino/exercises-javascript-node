8c11b55d57d5fa666f21524c768fb656a592e78a
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2019-10-25 11:20:52 +0200

    Class 6. Exercise 6 solved (Else/If).

--- clase-6/ejercicios/ejercicio_6.js
+++ clase-6/ejercicios/ejercicio_6.js
@@ -6,3 +6,25 @@
 // 	// Solución con operador ternario
 // Usando Switch
 // 	// Solución con switch
+
+
+
+function numberEvenOdd(num1) {
+    if (isNaN(num1) || num1 === "undefined" || num1 === null || num1 === "") {
+        alert("Inserta un numero");
+    } else {
+
+        // - Resuelto con if/else if
+        if (num1%2 === 0) {
+            alert("Tu numero es par: " + num1);
+        } else {
+            alert("Tu numero es impar " + num1);
+        }
+
+    }
+}
+
+alert("Inserta un numero para saber si es par o impar: ");
+var num1 = prompt("Numero: ");
+
+numberEvenOdd(num1);
