5151e8e001a5cd75ae595cd63f4dc4b0b2d9523e
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2019-10-27 14:13:34 +0100

    Class 7. Exercise 5 solved (For)

--- class-7/ejercicios/ejercicio_5.js
+++ class-7/ejercicios/ejercicio_5.js
@@ -3,3 +3,23 @@
 // - Usando for:
 // - Usando while y break:
 // - Usando Do...While:
+
+
+
+
+// - Resuelto con for:
+function getOdd50(num) {
+  console.group("Numeros pares del " + num + "-" + (num + 50) + " con for: ");
+
+  for (let index = num; index <= (num + 50); index++) {
+    //console.log(index);
+
+    if (index % 2 !== 0) {
+      console.log("Numero " + index);
+    }
+  }
+
+  console.groupEnd("Numeros pares del " + num + "-" + (num + 50) + " con for: ");
+}
+
+getOdd50(50);
