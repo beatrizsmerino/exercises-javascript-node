a2321c99e480c39ed49591616d5078ce09b139bd
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2019-10-27 19:01:56 +0100

    Class 7. Exercise 8 solved.

--- class-7/ejercicios/ejercicio_9.js
+++
@@ -1,8 +0,0 @@
-// Ejecicio
-
-
-var lenguajes = [
-    { "nombre": "Javascript", "extension": ".js" },
-    { "nombre": "PHP", "extension": ".php" },
-    { "nombre": "PHP", "extension": ".php" },
-];
