6af22cc5025719d122ad34a1d816af9d6640c195
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2019-10-27 17:59:28 +0100

    Class 7. Exercise 6 solved (Do...While)

--- class-7/ejercicios/ejercicio_6.js
+++ class-7/ejercicios/ejercicio_6.js
@@ -41,6 +41,7 @@ getOdd50(10);
 
 
 // - Resuelto con while:
+/*
 function getOdd50(num) {
   console.group("Numeros del " + num + "-" + (num + 50) + " con while: ");
 
@@ -69,5 +70,36 @@ function getOdd50(num) {
 }
 
 getOdd50(30);
+*/
 
 
+
+// - Resuelto con Do...While:
+function getOdd50(num) {
+  console.group("Numeros del " + num + "-" + (num + 50) + " con while: ");
+
+  var sumOdd = 0;
+  var sumOddOperation = "";
+  let index = num;
+
+  do {
+    // Numeros pares
+    if (index % 2 === 0) {
+      console.log("%cNumero " + index, "padding: 0.1rem 0.5rem; background-color: #454545; color: #f3f3f3;");
+    }
+
+    // Numeros impares
+    if (index % 2 !== 0) {
+      sumOdd += index;
+      sumOddOperation += index + " + ";
+    }
+
+    index++;
+  } while (index <= (num + 50));
+
+  sumOddOperation = sumOddOperation.slice(0, -2) + " = ";
+  console.log("Suma total de numeros impares: \n" + sumOddOperation + sumOdd);
+  console.groupEnd("Numeros del " + num + "-" + (num + 50) + " con while: ");
+}
+
+getOdd50(0);
