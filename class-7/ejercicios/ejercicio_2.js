c9b82fb0f3e0778a318e1cb16cc9eb8646db7ee2
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2019-10-27 12:29:46 +0100

    Class 7. Exercise 2 solved (While)

--- class-7/ejercicios/ejercicio_2.js
+++ class-7/ejercicios/ejercicio_2.js
@@ -13,3 +13,13 @@ for (let index = 0; index < 100; index++) {
   console.log("Numero " + (index + 1));
 }
 console.groupEnd("Numeros del 1-100 con for: ");
+
+// - Resuelto con while:
+console.group("Numeros del 1-100 con while: ");
+let counter = 0;
+while (counter < 100) {
+  console.log("Numero " + (counter + 1));
+  counter++
+}
+console.groupEnd("Numeros del 1-100 con while: ");
+
