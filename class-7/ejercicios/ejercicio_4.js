935281b9e84f3cdc99959c8b230665c7ffb3793a
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2019-10-27 13:24:50 +0100

    Class 7. Exercise 4 solved (For)

--- class-7/ejercicios/ejercicio_4.js
+++ class-7/ejercicios/ejercicio_4.js
@@ -3,3 +3,14 @@
 // - Usando for:
 // - Usando while y break:
 // - Usando Do...While:
+
+
+
+// - Resuelto con for:
+console.group("Numeros pares del 0-100 con for: ");
+for (let index = 0; index <= 100; index += 2) {
+  console.log("Numero " + index);
+}
+console.groupEnd("Numeros pares del 0-100 con for: ");
+
+
