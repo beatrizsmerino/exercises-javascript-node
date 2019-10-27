9ca47dd8146b1245f388982b6868fb9e0a9ba34c
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2019-10-27 12:29:23 +0100

    Class 7. Exercise 2 solved (For)

--- class-7/ejercicios/ejercicio_2.js
+++ class-7/ejercicios/ejercicio_2.js
@@ -3,3 +3,13 @@
 // - Usando for:
 // - Usando while y break:
 // - Usando Do...While:
+
+
+
+
+// - Resuelto con for:
+console.group("Numeros del 1-100 con for: ");
+for (let index = 0; index < 100; index++) {
+  console.log("Numero " + (index + 1));
+}
+console.groupEnd("Numeros del 1-100 con for: ");
