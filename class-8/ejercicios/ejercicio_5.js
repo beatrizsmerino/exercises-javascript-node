cda8a299ea2e4621bd1b2feb81d84e76d7a90c14
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2019-10-28 21:59:18 +0100

    Class 8. Exercise 5 solved.

--- class-8/ejercicios/ejercicio_5.js
+++ class-8/ejercicios/ejercicio_5.js
@@ -4,20 +4,54 @@
 
 
 function sinceMaster() {
-    let nowDate = new Date();
+  // HOY
+  ////////////////////////////
+  let nowDate = new Date();
+  console.info("Hoy: \n" + nowDate.toLocaleString());
 
-    // 2/10/19 - 29/06/20
-    // 16:00 - 19:00
 
-    const masterStartDay = 2;
-    const masterStartMonth = 10;
-    const masterStartYear = 2019;
 
-    const masterStart = new Date(Date.UTC(masterStartYear, masterStartMonth, masterStartDay));
+  // INICIO DEL MASTER
+  ////////////////////////////
+  // 2/10/19 - 29/06/20
+  // 16:00 - 19:00
 
-    let hoursSinceMaster = nowDate.setDate(nowDate.getDate());
-    console.log(hoursSinceMaster);
-    let daysSinceMaster = nowDate.setDate(nowDate.getDate() + days);
+  const masterStartDay = {
+    "seconds": 00,
+    "minutes": 00,
+    "hours": 16,
+    "day": 2,
+    "month": 9, // El mes se representa de 0 a 11
+    "year": 2019
+  };
+
+  const masterStart = new Date(masterStartDay.year, masterStartDay.month, masterStartDay.day, masterStartDay.hours, masterStartDay.minutes, masterStartDay.seconds);
+  console.info("Inicio del Master: \n" + masterStart);
+
+  // 1000
+  // (60 * 60 * 1000)
+  // (60 * 1000)
+  // (24 * 60 * 60 * 1000)
+
+  // Milisegundos
+  let ms = nowDate - masterStart;
+  console.info("Milisegundos: \n" + ms);
+
+  // Segundos
+  let seg = Math.floor(ms / 1000);
+  console.info("Segundos: \n" + seg);
+
+  // Minutos
+  let mm = Math.floor(seg / 60);
+  console.info("Minutos: \n" + mm);
+
+  // Horas
+  let h = Math.floor(mm / 60);
+  console.info("Horas: \n" + h);
+
+  // Dias
+  let d = Math.floor(h / 24);
+  console.info("Dias: \n" + d);
 }
 
 sinceMaster();
