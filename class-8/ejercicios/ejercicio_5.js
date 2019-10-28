e5fb70e062228aaf6ba511b39ae44b60490e65f4
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2019-10-28 15:02:33 +0100

    Class 8. Exercise 5. Working

--- class-8/ejercicios/ejercicio_5.js
+++ class-8/ejercicios/ejercicio_5.js
@@ -1,1 +1,23 @@
-// 5 - ¿Cuantas horas han pasado desde que emepezó este master? y... ¿en días?
+// 5 - ¿Cuantas horas han pasado desde que empezó este master? y... ¿en días?
+
+
+
+
+function sinceMaster() {
+    let nowDate = new Date();
+
+    // 2/10/19 - 29/06/20
+    // 16:00 - 19:00
+
+    const masterStartDay = 2;
+    const masterStartMonth = 10;
+    const masterStartYear = 2019;
+
+    const masterStart = new Date(Date.UTC(masterStartYear, masterStartMonth, masterStartDay));
+
+    let hoursSinceMaster = nowDate.setDate(nowDate.getDate());
+    console.log(hoursSinceMaster);
+    let daysSinceMaster = nowDate.setDate(nowDate.getDate() + days);
+}
+
+sinceMaster();
