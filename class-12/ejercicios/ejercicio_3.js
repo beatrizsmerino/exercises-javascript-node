035be5f8a600393fed85a7498dde5191d2ab854d
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2019-10-31 00:32:48 +0100

    Class 12. Exercise 3 solved.

--- class-12/ejercicios/ejercicio_3.js
+++ class-12/ejercicios/ejercicio_3.js
@@ -1,4 +1,19 @@
 // 3 - Diseña un script que sustituya todas las imágenes de las entradas de Tecnología del Mundo Today por imágenes dummy de gatitos.
 
 // https://www.elmundotoday.com/noticias/tecnologia/
-// https://http.cat/
+// https://placekitten.com/
+
+function changeImgs() {
+  var images = document.querySelectorAll(".td-big-grid-wrapper .td-big-grid-post");
+  console.log(images);
+
+  let array = [];
+  for (let index = 0; index < images.length; index++) {
+    const image = images[index].querySelectorAll(".td-image-wrap .entry-thumb")[0];
+    array.push(image.src);
+    image.src = "http://placekitten.com/400/400";
+  }
+  console.log(array);
+
+}
+changeImgs();
