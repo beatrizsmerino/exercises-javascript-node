d0202eaf0779befb9d8adfbfccf746e7784b7973
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2019-11-06 19:15:14 +0100

    Class 14. Working...

--- class-14/ejercicios/ejercicio_2.js
+++ class-14/ejercicios/ejercicio_2.js
@@ -1,29 +1,51 @@
-// Hacer un botón que esconda/enseñe el texto de un parrafo cada vez que se pulsa el mismo boton. Por defecto, el párrafo debe estar visible// Hacer un botón que esconda/enseñe el texto de un parrafo cada vez que se pulsa el mismo boton. Por defecto, el párrafo debe estar visible// Hacer un botón que esconda/enseñe el texto de un parrafo cada vez que se pulsa el mismo boton. Por defecto, el párrafo debe estar visible
+// Hacer un botón que esconda/enseñe el texto de un parrafo cada vez que se pulsa el mismo boton. Por defecto, el párrafo debe estar visible
 
 function buttonShowViewParagraph() {
-    // Create button
-    var body = document.getElementsByTagName("body")[0];
-    var buttonElem = document.createElement("button");
-    var buttonTextElem = document.createTextNode("Click Me");
-    buttonElem.setAttribute("id", "myButton");
-    buttonElem.appendChild(buttonTextElem);
-    body.appendChild(buttonElem);
+	// Create button
+	var body = document.getElementsByTagName("body")[0];
+	var buttonElem = document.createElement("button");
+	var buttonTextElem = document.createTextNode("Click Me");
+	buttonElem.setAttribute("id", "myButton");
+	buttonElem.appendChild(buttonTextElem);
+	body.appendChild(buttonElem);
 
-    // Create paragraph
-    var paragraphElem = document.createElement("p");
-    var paragraphTextElem = document.createTextNode("Paragraph");
-    paragraphElem.setAttribute("id", "myParagraph");
-    paragraphElem.appendChild(paragraphTextElem);
-    body.appendChild(paragraphElem);
+	// Create paragraph
+	var paragraphElem = document.createElement("p");
+	var paragraphTextElem = document.createTextNode("Paragraph");
+	paragraphElem.setAttribute("id", "myParagraph");
+	paragraphElem.appendChild(paragraphTextElem);
+	body.appendChild(paragraphElem);
 
-    var buttonDom = document.getElementById("myButton");
-    var paragraphDom = document.getElementById("myParagraph");
+	var buttonDom = document.getElementById("myButton");
+	var paragraphDom = document.getElementById("myParagraph");
 
-    // Solved width classList.toggle()
-    buttonDom.addEventListener("click", function() {
-        paragraphDom.classList.toggle("is-view");
-        console.log(paragraphDom);
-    });
+	// Solved width classList.toggle()
+	buttonDom.addEventListener("click", function () {
+		paragraphDom.classList.toggle("is-view");
+		console.log(paragraphDom);
+	});
+
+
+	// Solved variable state
+	function showHide(element, targetEvent) {
+		function toggleClass() {
+			var state = false;
+
+			if (state) {
+				element.classList.remove("is-view");
+				state = false;
+				console.log(state);
+			} else {
+				element.classList.add("is-view");
+				state = true;
+				console.log(state);
+			}
+		}
+		targetEvent.onclick = toggleClass;
+	}
+
+	showHide(paragraphDom, buttonDom);
+
 }
 
 buttonShowViewParagraph();
