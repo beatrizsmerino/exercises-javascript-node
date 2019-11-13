d761a9ecce9c7ab4235d28e5eae90baf05d851ce
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2019-11-13 19:17:18 +0100

    Added files class-18

---
+++ class-18/clase/scripts.js
@@ -0,0 +1,14 @@
+
+// POSTMAN -> https://www.getpostman.com/downloads/
+// JSON -> https://jsonplaceholder.typicode.com/guide.html
+
+
+function sendForm(){
+    alert("Se ha enviado");
+    var user = document.getElementById("user");
+    var pass = document.getElementById("pass");
+
+}
+
+
+button.addEventListener("click", sendForm());
