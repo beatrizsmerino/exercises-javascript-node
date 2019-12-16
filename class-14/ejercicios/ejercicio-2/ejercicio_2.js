19d2693f66c3263d277bdfd1b64d059dfab534d7
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2019-12-17 00:51:43 +0100

    Class 14. Bug fixed in the exercise 2.

--- class-14/ejercicios/ejercicio-2/ejercicio_2.js
+++ class-14/ejercicios/ejercicio-2/ejercicio_2.js
@@ -1,4 +1,5 @@
-// Hacer un botón que esconda/enseñe el texto de un parrafo cada vez que se pulsa el mismo boton. Por defecto, el párrafo debe estar visible
+// Hacer un botón que esconda/enseñe el texto de un parrafo cada vez que se pulsa el mismo boton.
+// Por defecto, el párrafo debe estar visible
 
 function buttonShowViewParagraph() {
 	// Create button
@@ -19,33 +20,21 @@ function buttonShowViewParagraph() {
 	var buttonDom = document.getElementById("myButton");
 	var paragraphDom = document.getElementById("myParagraph");
 
+
+
+
 	// Solved width classList.toggle()
 	buttonDom.addEventListener("click", function () {
 		paragraphDom.classList.toggle("is-view");
+
+		if (paragraphDom.classList.contains("is-view")) {
+			paragraphDom.style.display = "block";
+		} else {
+			paragraphDom.style.display = "none";
+		}
+
 		console.log(paragraphDom);
 	});
-
-
-	// Solved variable state
-	function showHide(element, targetEvent) {
-		function toggleClass() {
-			var state = false;
-
-			if (state) {
-				element.classList.remove("is-view");
-				state = false;
-				console.log(state);
-			} else {
-				element.classList.add("is-view");
-				state = true;
-				console.log(state);
-			}
-		}
-		targetEvent.onclick = toggleClass;
-	}
-
-	showHide(paragraphDom, buttonDom);
-
 }
 
 buttonShowViewParagraph();
