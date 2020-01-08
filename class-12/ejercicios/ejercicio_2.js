bcf0907b9fbc418f9812a257c1362c347fb2429f
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2020-01-08 18:31:34 +0100

    Class 12. Exercise 2 solved.

--- class-12/ejercicios/ejercicio_2.js
+++ class-12/ejercicios/ejercicio_2.js
@@ -6,22 +6,54 @@
 // https://www.metromadrid.es/es
 
 function subway() {
-  let array = [];
-  let lines = document.querySelectorAll(".list__otraslineas .list__lineas__element");
+    let array = [];
+    let lines = document.querySelectorAll(
+        ".list__otraslineas .list__lineas__element"
+    );
 
-  for (let index = 0; index < lines.length; index++) {
-    const line = lines[index];
-    console.log(line);
-    let lineName = array.push(line.querySelectorAll("a img")[0].class);
-    let lineStatus = array.push(line.querySelectorAll("a span[class*=state]")[0]);
+    // console.dir(lines);
 
-    array.push({
-      "name": lineName,
-      "status": lineStatus
-    });
-  }
-  console.log(array);
+    for (let index = 0; index < lines.length; index++) {
+        let line = lines[index];
 
+        let lineName = "";
+        // let lineName = lines.querySelectorAll("a img")[0].class;
+        // let lineLink = line.children[0].children[0];
+        let lineLink = line.getElementsByTagName("img")[0];
+        if (typeof lineLink !== "undefined") {
+            lineName = lineLink.className;
+        }
+        //console.log(lineName);
+
+        // let lineStatus = line.querySelectorAll("a span[class*=state]")[0];
+        let lineSpan = line.children[1];
+
+        let lineStatus = "";
+        if (typeof lineSpan !== "undefined") {
+            lineStatusClass = lineSpan.className;
+            switch (lineStatusClass) {
+                case " state--green ":
+                    lineStatus = "Estación en buen estado";
+                    break;
+                case "top has-tip":
+                    lineStatus = document.getElementById(lineSpan.dataset["toggle"]).innerText;
+                    break;
+                default:
+                    break;
+            }
+        }
+        // console.log(lineStatus);
+
+        let arrayIndex = {
+            id: index,
+            name: lineName,
+            status: lineStatus
+        };
+
+        array.push(arrayIndex);
+    }
+
+    console.info("Estados de las lineas de metro", array);
 }
 
 subway();
