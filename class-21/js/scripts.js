6cabfc61e0a71427452aedd1067f4166f86f7d1f
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2020-01-10 07:47:30 +0100

    Class 21. Exercise Github (get email of user) solved

--- class-21/js/scripts.js
+++ class-21/js/scripts.js
@@ -4,7 +4,6 @@
 
 let urlAPI = "https://api.github.com/users/";
 let urlEventsAPI = "/events";
-let bodyDom = document.body;
 let searchDom = document.getElementById("search");
 let searchButtonDom = document.getElementById("button");
 
@@ -18,10 +17,17 @@ let color = {
 };
 
 let stylesConsole = `
-    padding: 0.5rem 1rem;
+    padding: 0.4rem .8rem;
     color: ${color.color_brand_2};
     background-color: ${color.color_brand_1};
 `;
+
+
+let user = {
+    "name": "",
+    "login": "",
+    "email": ""
+}
 
 
 // TOOLS
@@ -35,59 +41,59 @@ function delay(fn, ms) {
 }
 
 function svgMe() {
-	let images = document.querySelectorAll("img.svgMe");
+    let images = document.querySelectorAll("img.svgMe");
 
-	// console.info("Array of images -> ", images);
+    // console.info("Array of images -> ", images);
 
-	images.forEach(image => {
-		let imgId = image.getAttribute("id");
-		let imgClass = image.getAttribute("class");
-		let imgUrl = image.getAttribute("src");
+    images.forEach(image => {
+        let imgId = image.getAttribute("id");
+        let imgClass = image.getAttribute("class");
+        let imgUrl = image.getAttribute("src");
 
-		let request = new XMLHttpRequest();
-		request.onreadystatechange = function () {
-			if (request.readyState == 4 && request.status == 200) {
-				// console.info("request in xml -> ", request.responseXML);
-				callback(request.responseXML);
-			}
-		};
+        let request = new XMLHttpRequest();
+        request.onreadystatechange = function () {
+            if (request.readyState == 4 && request.status == 200) {
+                // console.info("request in xml -> ", request.responseXML);
+                callback(request.responseXML);
+            }
+        };
 
-		function callback(requestXML) {
-			let imgSvg = requestXML.querySelector("svg");
+        function callback(requestXML) {
+            let imgSvg = requestXML.querySelector("svg");
 
-			// console.info("data type of 'data' -> ", typeof requestXML);
-			// console.info("'data' -> ", requestXML);
-			// console.info("images with svgMe -> ", imgSvg);
+            // console.info("data type of 'data' -> ", typeof requestXML);
+            // console.info("'data' -> ", requestXML);
+            // console.info("images with svgMe -> ", imgSvg);
 
-			if (typeof imgId !== "undefined") {
-				// console.info(imgId);
-				imgSvg.setAttribute("id", imgId);
-			}
+            if (typeof imgId !== "undefined") {
+                // console.info(imgId);
+                imgSvg.setAttribute("id", imgId);
+            }
 
-			if (typeof imgClass !== "undefined") {
-				// console.info(imgClass);
-				imgSvg.setAttribute("class", imgClass);
-				imgSvg.classList.add("svgMe--replaced");
-			}
+            if (typeof imgClass !== "undefined") {
+                // console.info(imgClass);
+                imgSvg.setAttribute("class", imgClass);
+                imgSvg.classList.add("svgMe--replaced");
+            }
 
-			imgSvg.removeAttribute("xmlns:a");
-			if (
-				!imgSvg.getAttribute("viewBox") &&
-				imgSvg.getAttribute("height") &&
-				imgSvg.getAttribute("width")
-			) {
-				imgSvg.setAttribute(
-					"viewBox",
-					"0 0 " + imgSvg.getAttribute("height") + " " + imgSvg.getAttribute("width")
-				);
-			}
+            imgSvg.removeAttribute("xmlns:a");
+            if (
+                !imgSvg.getAttribute("viewBox") &&
+                imgSvg.getAttribute("height") &&
+                imgSvg.getAttribute("width")
+            ) {
+                imgSvg.setAttribute(
+                    "viewBox",
+                    "0 0 " + imgSvg.getAttribute("height") + " " + imgSvg.getAttribute("width")
+                );
+            }
 
-			image.replaceWith(imgSvg);
-		}
+            image.replaceWith(imgSvg);
+        }
 
-		request.open("GET", imgUrl);
-		request.send();
-	});
+        request.open("GET", imgUrl);
+        request.send();
+    });
 }
 svgMe();
 
@@ -95,9 +101,9 @@ svgMe();
 // AJAX HANDLER - FETCH
 //////////////////////////////////
 function ajaxHandler(url, action) {
-    console.info(url);
+    // console.info(url);
 
-    addLoader(bodyDom);
+    addLoader();
 
     fetch(url)
         .then(function (response) {
@@ -105,9 +111,10 @@ function ajaxHandler(url, action) {
         })
         .then(function (data) {
             let timer = setInterval(function () {
-                removeLoader(bodyDom);
+                removeLoader();
                 setAction(action, data);
                 clearInterval(timer);
+                return data;
             }, 5000);
         })
         .catch(function (error) {
@@ -117,129 +124,212 @@ function ajaxHandler(url, action) {
 
 // LOADER
 //////////////////////////////////
-function addLoader(elementDom) {
-    let loader = document.createElement("div");
-    loader.setAttribute("id", "loader");
-    loader.setAttribute("class", "loader");
-    loader.style.backgroundImage = "url('images/loader.gif')";
-    elementDom.appendChild(loader);
+function addLoader() {
+    let loader = document.getElementById("loader");
+    if (!loader) {
+        let loader = document.createElement("div");
+        loader.setAttribute("id", "loader");
+        loader.setAttribute("class", "loader");
+        loader.style.backgroundImage = "url('images/loader.gif')";
+        document.body.appendChild(loader);
+    }
 }
 
-function removeLoader(elementDom) {
+function removeLoader() {
     let loader = document.getElementById("loader");
-    elementDom.removeChild(loader);
+    if (loader) {
+        document.body.removeChild(loader);
+    }
 }
 
 // INSERT DATA
 //////////////////////////////////
 function setAction(action, responseData) {
     switch (action) {
-        case "insertData":
-            insertData(responseData);
+        case "setUserData":
+            setUserData(responseData);
             break;
-        case "insertDataEvents":
-            insertDataEvents(responseData);
+        case "getUserEmail":
+            getUserEmail(responseData);
             break;
         default:
             break;
     }
 }
 
-function insertData(responseData) {
+function templatePopup() {
     let popup = document.createElement("div");
     popup.setAttribute("id", "popup");
-    console.table(responseData);
-
-    let content = document.createElement("div");
-    content.setAttribute("class", "dataUser");
-
-    for (const key in responseData) {
-        const element = responseData[key];
-        // console.log(element);
-
-        let strong = document.createElement("strong");
-        let strongText = document.createTextNode(key + ": ");
-        let paragraph = document.createElement("p");
-        let paragraphText = document.createTextNode(element);
-
-        strong.appendChild(strongText);
-        paragraph.appendChild(strong);
-        paragraph.appendChild(paragraphText);
-        content.appendChild(paragraph);
-    }
-
-    // console.dir(content);
 
     let template = `
         <div class="popup">
             <div class="popup__inner">
                 <div id="popupClose" class="popup__close"></div>
-                <div class="scroll">
-                    <div class="scroll__inner">
-                        <div class="popup__content scroll__content">
-                        </div>
-                    </div>
+                    <div class="popup__content"></div>
                 </div>
             </div>
         </div>
-    `;
+	`;
 
     popup.innerHTML = template;
-    bodyDom.appendChild(popup);
+    document.body.appendChild(popup);
 
-    document.getElementsByClassName("popup__content")[0].appendChild(content);
     document.getElementById("popupClose").addEventListener("click", function () {
         removeData();
     });
 }
 
-function removeData() {
-    let popup = document.getElementById("popup");
-    bodyDom.removeChild(popup);
-}
+function setUserData(responseData) {
+    console.table("setUserData: ", responseData);
 
-function insertDataEvents(responseData) {
-    console.log("%cfunction->insertDataEvents", `; ${stylesConsole}`);
-    console.table(responseData);
-
-    let popup = document.querySelector(".popup__content");
-
-    let eventsDom = document.createElement("div");
-    eventsDom.setAttribute("class", "dataEvents");
-
-    for (const key in responseData) {
-        const element = responseData[key];
-        let eventItemDom = document.createElement("div");
-
-        eventItemTextDom = document.createTextNode(element);
-        eventItemDom.setAttribute("class", "dataEvents__item");
-
-        for (const key in element) {
-            const element2 = element[key];
-            if (element.hasOwnProperty(key)) {
-
-            }
-        }
-
-        eventItemDom.appendChild(eventItemTextDom);
-        eventsDom.appendChild(eventItemDom);
+    if (!document.getElementById("popup")) {
+        templatePopup();
     }
 
-    popup.appendChild(eventsDom);
+    if (responseData) {
+        let userData = document.createElement("div");
+        userData.classList.add("user-data");
+        userData.setAttribute("id", "userData");
+        document.getElementsByClassName("popup__content")[0].appendChild(userData);
+
+        // Si el nombre de usuario existe
+        if (
+            responseData.login !== null &&
+            responseData.login !== "undefined"
+        ) {
+            user.login = responseData.login;
+            user.name = responseData.name;
+
+            // Si encuentra su correo
+            if (responseData.email !== null) {
+                user.email = responseData.email;
+                insertUserData();
+
+                // Si no lo encuentra busca mas a fondo en los eventos del usuario	
+            } else {
+                ajaxHandler(urlAPI + user.login + urlEventsAPI, "getUserEmail");
+            }
+        } else {
+            insertUserData();
+        }
+    }
 }
+
+function removeData() {
+    let popup = document.getElementById("popup");
+    if (popup) {
+        document.body.removeChild(popup);
+    }
+
+    user.name = "";
+    user.login = "";
+    user.email = "";
+}
+
+function getUserEmail(responseData) {
+    console.table("getUserEmail: ", responseData);
+
+    for (let i = 0; i < responseData.length; i++) {
+        let element = responseData[i];
+        // let userEmail = element.payload.commits[0].author.email;
+
+        // console.log(user.email);
+        if (user.email === "") {
+            if (element.hasOwnProperty("payload")) {
+                let payload = element.payload;
+                if (payload.hasOwnProperty("commits")) {
+                    for (let j = 0; j < element.payload.commits.length; j++) {
+                        let commit = element.payload.commits[j];
+                        if (commit.hasOwnProperty("author")) {
+                            let author = commit.author;
+                            if (author.hasOwnProperty("name")) {
+                                let userName = author.name;
+
+                                if (userName == user.name) {
+                                    if (author.hasOwnProperty("email")) {
+                                        let userEmail = author.email;
+                                        // console.info(userEmail);
+
+                                        // console.log(userEmail.search("noreply"));
+                                        if (userEmail.search("noreply") == -1) {
+                                            user.email = userEmail;
+                                            break;
+                                        } else {
+                                            continue;
+                                        }
+
+                                    } else {
+                                        user.email = "Email not found";
+                                    }
+                                } else {
+                                    continue;
+                                }
+                            } else {
+                                continue;
+                            }
+                        } else {
+                            continue;
+                        }
+                    }
+                } else {
+                    continue;
+                }
+
+            } else {
+                continue;
+            }
+        } else {
+            break;
+        }
+    }
+
+    insertUserData();
+}
+
+function insertUserData() {
+    console.info("%cUser:", stylesConsole);
+    console.info(user);
+
+    let templateUserEmail = "";
+    if (user.email !== "Email not found") {
+        templateUserEmail = `<a href="mailto:${user.email}">
+								${user.email}
+							</a>`;
+    } else {
+        templateUserEmail = user.email;
+    }
+
+    let templateUser = `
+					<div class="user-data__inner">
+						<p class="user-data__login">
+							<a href="http://github.com/${user.login}" target="_blank">
+								${user.login}
+							</a>
+						</p>
+						<p class="user-data__name">
+							${user.name}
+						</p>
+						<p class="user-data__email">
+							${templateUserEmail}
+						</p>
+					</div>
+				`;
+
+    let userData = document.getElementById("userData");
+    userData.innerHTML = templateUser;
+    error404(userData);
+}
+
+function error404(userData) {
+    if (user.login === "" || user.name === "" || user.email === "") {
+        userData.classList.add("is-error404");
+    }
+}
+
 
 function init(searchValue) {
     searchButtonDom.classList.add("is-searching");
-    ajaxHandler(urlAPI + searchValue, "insertData");
-
-    let timerPopUp = setInterval(function () {
-        let popup = document.querySelector(".popup__content");
-        if (popup) {
-            console.log("Existe popup");
-            clearInterval(timerPopUp);
-            ajaxHandler(urlAPI + searchValue + urlEventsAPI, "insertDataEvents");
-        }
-    }, 300);
+    ajaxHandler(urlAPI + searchValue, "setUserData");
 }
 
 searchDom.addEventListener(
@@ -248,4 +338,6 @@ searchDom.addEventListener(
         init(this.value);
     }, 2000)
 );
-searchButtonDom.addEventListener("click", init(searchDom.value));
+searchButtonDom.addEventListener("click", function () {
+    init(searchDom.value);
+});
