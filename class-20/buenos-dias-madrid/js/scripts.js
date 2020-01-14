0e516443cd84ef8e9a65415eac3829c3fc0a65d0
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2020-01-14 14:40:52 +0100

    Class 20. Exercise 'Buenos día Madrid!'. Added files and instructions.

---
+++ class-20/buenos-dias-madrid/js/scripts.js
@@ -0,0 +1,22 @@
+
+// API airemad
+const aireMadAPI = "http://airemad.com/#/";
+
+const madridPollutionAPI = "http://airemad.com/api/v1/pollution";
+const madridPollenAPI = "http://airemad.com/api/v1/pollen";
+const madridAcusticAPI = "http://airemad.com/api/v1/acustic";
+const madridFluAPI = "http://airemad.com/api/v1/flu";
+const madridWeatherAPI = "http://airemad.com/api/v1/weather";
+
+
+// API infor
+const madridCameraAPI = "http://informo.munimadrid.es/cameras/Camara";
+
+
+// Estado de la contaminación en Madrid
+
+// Estado del tiempo ahora mismo en Madrid
+
+// Previsión meteorológica de los próximos 4/5 días en Madrid
+
+// Una imagen aleatoria del trafico de la ciudad usando las cámaras abiertas de la ciudad.
