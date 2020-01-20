9e10e6fd358befde4f9391544043b406ea2332e0
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2020-01-20 14:41:03 +0100

    Class 26. Exercise promises (pokemon). Added feature getPokemon.

--- class-26/promises/js/scripts.js
+++ class-26/promises/js/scripts.js
@@ -1,109 +1,141 @@
 // Ejemplo PROMISE
 const getCountPokemon = () => {
-	return new Promise((resolve, reject) => {
-		const url = `https://pokeapi.co/api/v2/pokemon/`;
-		fetch(url)
-			.then(response => {
-				// console.log(`Estado del Servidor: ${response.status === 200 ? "OK" : "Error"}`);
-				// console.log(response);
-				resolve(response.json());
-			})
-			.catch((e) => reject(`Error: ${e}`));
-	});
+    return new Promise((resolve, reject) => {
+        const url = `https://pokeapi.co/api/v2/pokemon/`;
+        fetch(url)
+            .then(response => {
+                // console.log(`Estado del Servidor: ${response.status === 200 ? "OK" : "Error"}`);
+                // console.log(response);
+                resolve(response.json());
+            })
+            .catch((e) => reject(`Error: ${e}`));
+    });
 };
 
 
 const getAllPokemon = (totalNumber) => {
-	const url = `https://pokeapi.co/api/v2/pokemon/?offset=0&limit=${totalNumber}`;
-	// console.log(url);
+    const url = `https://pokeapi.co/api/v2/pokemon/?offset=0&limit=${totalNumber}`;
+    // console.log(url);
 
-	return new Promise((resolve, reject) => {
-		fetch(url)
-			.then(response => {
-				resolve(response.json());
-			})
-			.catch((e) => reject(`Error: ${e}`));
-        /*
-        .then(
-            response => {
-                console.log(response);
-                response.json();
-            },
-            e => console.log(`Error capturado: ${e}`)
-        )
-        */
-	});
+    return new Promise((resolve, reject) => {
+        fetch(url)
+            .then(response => {
+                resolve(response.json());
+            })
+            .catch((e) => reject(`Error: ${e}`));
+    });
 }
 
 
+const getPokemon = (pokemonUrl) => {
+    return new Promise((resolve, reject) => {
+        const url = pokemonUrl;
+        console.log("getPokemon(pokemonUrl)", url);
 
+        fetch(url)
+            .then(response => {
+                resolve(response.json());
+            })
+            .catch((e) => reject(`Error: ${e}`));
+    });
+}
 
 
 function getAllName(array) {
-	// console.log(array);
-	let pokemonListDom = document.createElement("article");
-	pokemonListDom.setAttribute("class", "pokemon-list");
-	pokemonListDom.setAttribute("id", "pokemonList");
+    // console.log(array);
+    let pokemonListDom = document.createElement("article");
+    pokemonListDom.setAttribute("class", "pokemon-list");
+    pokemonListDom.setAttribute("id", "pokemonList");
 
-	for (let index = 0; index < array.length; index++) {
-		const object = array[index];
-		// console.log(object);
+    for (let index = 0; index < array.length; index++) {
+        const object = array[index];
+        // console.log(object);
 
-		let pokemonDom = document.createElement("span");
-		pokemonDom.setAttribute("class", "pokemon-tag");
-		pokemonDom.setAttribute("data-url", object["url"]);
+        let pokemonDom = document.createElement("span");
+        pokemonDom.setAttribute("class", "pokemon-tag");
+        pokemonDom.setAttribute("data-url", object["url"]);
 
-		let pokemonTextDom = document.createTextNode("#" + (index + 1) + " " + object["name"]);
+        let pokemonTextDom = document.createTextNode("#" + (index + 1) + " " + object["name"]);
 
-		pokemonDom.appendChild(pokemonTextDom);
-		pokemonListDom.appendChild(pokemonDom);
+        pokemonDom.appendChild(pokemonTextDom);
+        pokemonListDom.appendChild(pokemonDom);
 
-	}
+    }
 
-	document.getElementById("pageMain").appendChild(pokemonListDom);
+    document.getElementById("pageSection").appendChild(pokemonListDom);
 
-	let pokemonTag = document.getElementsByClassName("pokemon-tag");
-	for (let index = 0; index < pokemonTag.length; index++) {
-		const element = pokemonTag[index];
-		element.addEventListener("click", function () {
-			let pokemonUrl = this.getAttribute("data-url");
+    let pokemonTag = document.getElementsByClassName("pokemon-tag");
+    for (let index = 0; index < pokemonTag.length; index++) {
+        const element = pokemonTag[index];
+        element.addEventListener("click", function () {
+            let pokemonUrl = this.getAttribute("data-url");
+            console.log(pokemonUrl);
 
-			getPokemon(pokemonUrl);
-		});
-	}
+            getPokemon(pokemonUrl)
+                .then(result => {
+                    console.log("result", result);
+                    const pokemonData = {
+                        name: result.name,
+                        weight: result.weight,
+                        height: result.height
+                    };
+                    console.log(pokemonData.name);
+
+                    let pokemonElem = document.createElement("div");
+                    pokemonElem.setAttribute("id", "pokemonContent");
+                    pokemonElem.setAttribute("class", "pokemon-info");
+                    document.getElementById("pageSection").innerHTML = "";
+                    document.getElementById("pageSection").appendChild(pokemonElem);
+
+                    let pokemonContent = document.getElementById("pokemonContent");
+                    if (pokemonData && pokemonContent) {
+                        for (const key in pokemonData) {
+                            const element = pokemonData[key];
+                            let paragraphElem = document.createElement("p");
+
+                            let strongElem = document.createElement("strong");
+                            let strongTextElem = document.createTextNode(key + ":");
+
+                            let spanElem = document.createElement("span");
+                            let spanTextElem = document.createTextNode(element);
+
+                            strongElem.appendChild(strongTextElem);
+                            spanElem.appendChild(spanTextElem);
+                            paragraphElem.appendChild(strongElem);
+                            paragraphElem.appendChild(spanElem);
+                            pokemonContent.appendChild(paragraphElem);
+                        }
+                    }
+
+                });
+        });
+    }
 
 }
 
-function getPokemon(pokemonUrl) {
-	const url = pokemonUrl;
-	console.log(url);
-	fetch(url)
-		.then(response => console.log(response.json()))
-		.catch(e => console.log("Error: " + e));
-}
 
 
 getCountPokemon()
-	.then(response => {
-		// console.log(response);
-		let numberTotalPokemon = response.count;
-		console.log("Hay " + numberTotalPokemon + " pokemons");
+    .then(response => {
+        // console.log(response);
+        let numberTotalPokemon = response.count;
+        // console.log("Hay " + numberTotalPokemon + " pokemons");
 
-		let totalPokemonElem = document.createElement("p");
-		totalPokemonElem.setAttribute("class", "pokemon-total");
-		let totalPokemonTextElem = document.createTextNode("Total: " + numberTotalPokemon + " pokemons");
-		totalPokemonElem.appendChild(totalPokemonTextElem);
+        let totalPokemonElem = document.createElement("p");
+        totalPokemonElem.setAttribute("class", "pokemon-total");
+        let totalPokemonTextElem = document.createTextNode("Total: " + numberTotalPokemon + " pokemons");
+        totalPokemonElem.appendChild(totalPokemonTextElem);
 
-		document.getElementById("pageSection").prepend(totalPokemonElem);
+        document.getElementById("pageSection").prepend(totalPokemonElem);
 
-		getAllPokemon(numberTotalPokemon)
-			.then(
-				response => {
-					// console.log(response);
-					// console.log(response.results);
-					let jsonTotalPokemon = response.results;
-					getAllName(response.results);
-				}
-			)
+        getAllPokemon(numberTotalPokemon)
+            .then(
+                response => {
+                    // console.log(response);
+                    // console.log(response.results);
+                    let jsonTotalPokemon = response.results;
+                    getAllName(response.results);
+                }
+            )
 
-	});
+    });
