f19a495b5f663474e11e2352c455cf3ba666dff8
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2020-01-21 18:21:53 +0100

    Class 30. Exercise of router. Changed images of the examples 'page-js-master' folder. Changed styles, added google font  'Open Sans', added selector id to the pages.

--- class-30/clase/page.js-master/examples/state/app.js
+++ class-30/clase/page.js-master/examples/state/app.js
@@ -1,8 +1,8 @@
 
 var avatars = {
-  glottis: 'http://homepage.ntlworld.com/stureek/images/glottis03.jpg',
-  manny: 'http://kprojekt.net/wp-content/uploads/manny.jpg',
-  sal: 'http://homepage.ntlworld.com/stureek/images/sal01.jpg'
+    glottis: 'https://image.flaticon.com/icons/png/512/219/219961.png',
+    manny: 'https://un-app.herokuapp.com/assets/teacher-f4f3dd7acfd64b16aa51027159a6f5e8.png',
+    sal: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkovcVYkJI4zr3FkFjbROa8S7gkZ42FWLEaDxfIHPKBj8Xg_kg&s'
 };
 
 page.base('/state');
@@ -15,43 +15,43 @@ page();
 // just callbacks etc..
 
 function text(str) {
-  document.querySelector('p').textContent = str;
+    document.querySelector('p').textContent = str;
 }
 
 function index() {
-  text('Click a user below to load their avatar');
-  document.querySelector('img')
-    .style.display = 'none';
+    text('Click a user below to load their avatar');
+    document.querySelector('img')
+        .style.display = 'none';
 }
 
 function load(ctx, next) {
-  // check if we have .state.avatar already available
-  // this could for example be a cached html fragment.
-  if (ctx.state.avatar) {
-    ctx.avatar = ctx.state.avatar;
-    next();
-    return;
-  }
+    // check if we have .state.avatar already available
+    // this could for example be a cached html fragment.
+    if (ctx.state.avatar) {
+        ctx.avatar = ctx.state.avatar;
+        next();
+        return;
+    }
 
-  // pretend we're querying some database etc
-  setTimeout(function(){
-    // you can assign properties to the context
-    // for use between these functions. The .state
-    // property is what's saved in history.
-    ctx.state.avatar = ctx.avatar = avatars[ctx.params.name];
-    ctx.save();
-    next();
-  }, 600);
+    // pretend we're querying some database etc
+    setTimeout(function () {
+        // you can assign properties to the context
+        // for use between these functions. The .state
+        // property is what's saved in history.
+        ctx.state.avatar = ctx.avatar = avatars[ctx.params.name];
+        ctx.save();
+        next();
+    }, 600);
 }
 
 function show(ctx) {
-  var img = document.querySelector('img');
-  img.src = ctx.avatar;
-  img.style.display = 'block';
-  text('Showing ' + ctx.params.name);
+    var img = document.querySelector('img');
+    img.src = ctx.avatar;
+    img.style.display = 'block';
+    text('Showing ' + ctx.params.name);
 }
 
 function notfound() {
-  document.querySelector('p')
-    .textContent = 'not found';
+    document.querySelector('p')
+        .textContent = 'not found';
 }
--- class-30/ejercicios/routing/js/app-routes.js
+++ class-30/ejercicios/routing/js/app-routes.js
@@ -15,20 +15,24 @@ page('/');
 
 
 function getPageHome() {
-	document.getElementById("pageTitle").textContent = "Home";
-	getContentHome();
+    document.getElementsByClassName("page")[0].setAttribute("id", "pageHome");
+    document.getElementById("pageTitle").textContent = "Home";
+    getContentHome();
 }
 
 function getPageAllStations() {
-	document.getElementById("pageTitle").textContent = "Stations";
-	getContentAllStations();
+    document.getElementsByClassName("page")[0].setAttribute("id", "pageStations");
+    document.getElementById("pageTitle").textContent = "Stations";
+    getContentAllStations();
 }
 
 function getPageStation() {
-	document.getElementById("pageTitle").textContent = "Station";
-	getContentStation();
+    document.getElementsByClassName("page")[0].setAttribute("id", "pageStation");
+    document.getElementById("pageTitle").textContent = "Station";
+    getContentStation();
 }
 
 function getPageError404() {
-	document.getElementById("pageTitle").textContent = "Page not found. Error 404";
+    document.getElementsByClassName("page")[0].setAttribute("id", "pageError404");
+    document.getElementById("pageTitle").textContent = "Page not found. Error 404";
 }
