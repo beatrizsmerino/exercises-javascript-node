9d0ac8e5ae0e0cb08b8f95e9d1d672eef6f3f5d4
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2020-02-19 08:52:29 +0100

    Class 36. Graphic canvas of weather.
    Update and clean code.

---
+++ class-36/ejercicios/ejercicio-1/js/script.js
@@ -0,0 +1,28 @@
+/**
+ * @file Main file
+ * @author Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
+ * @copyright (2020)
+ */
+
+
+
+
+
+/**
+ * @requires geolocation
+ */
+import * as geolocation from './geolocation.js';
+
+
+
+
+
+/**
+ * @function functionAnonimAutoExecuted
+ * @description Anonymous auto executed function
+ * @see Used inside:
+ * @see - 'geolocation.js' -> {@link module:geolocation.set}
+ */
+(function () {
+	geolocation.set();
+})();
