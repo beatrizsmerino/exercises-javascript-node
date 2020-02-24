3eb24f37d36b17035f4e0f105b726af2d69d75b5
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2020-02-24 18:46:05 +0100

    Class 48. Node
    Added files of the class.
    - Execute a file js with node
    - Create a server with node

---
+++ class-48/clase/script.js
@@ -0,0 +1,9 @@
+// Ejecutar un archivo javascript desde la terminal con node.js
+// TERMINAL -> node + nameFile
+// node script.js
+
+console.log("Hola Mundo desde fichero js!");
+
+setTimeout(function () {
+    console.log("3 segundos despues");
+}, 3000);
