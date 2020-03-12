2645801ffd15da366b087fa0e2d3302f0c712098
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2020-03-12 14:34:06 +0100

    Class 35. Regex II: Ejercicios
    - Exercise 3. Solved.
    'Comprobar la seguridad de una contraseña'
    De esta forma comprobaremos:
    Contraseñas que contengan al menos una letra mayúscula.
    Contraseñas que contengan al menos una letra minúscula.
    Contraseñas que contengan al menos un número
    Contraseñas que contengan al menos un caracter especial @#$%.
    Contraseñas cuya longitud sea como mínimo 6 caracteres.
    Contraseñas cuya longitud máxima sea 20 caracteres.

--- class-35/ejercicios/ejercicio-3/js/script.js
+++ class-35/ejercicios/ejercicio-3/js/script.js
@@ -8,3 +8,21 @@
 // - Contraseñas que contengan al menos un caracter especial @#$%.
 // - Contraseñas cuya longitud sea como mínimo 6 caracteres.
 // - Contraseñas cuya longitud máxima sea 20 caracteres.
+
+
+const regexPassword = /(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=[0-9])(?=.*[@#$%]).{6,20}/g;
+
+const passwords = [
+	"Z%C2Uacgw_4weL@Q",
+	"QZ6UttU-&r4t%R+J",
+	"KK8a%K^9seQ$Qc8X",
+	"*Q#*9-CP%?JkXQSs",
+	"#1234abCD@", ,
+	"=G4T!v-J2_6aS^EW",
+	"perrito",
+	"perrito123",
+	"Perrito1234"
+];
+
+const validPassword = passwords.filter(password => password.match(regexPassword));
+console.log(validPassword);
