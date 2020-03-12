f4f465964bbd0fe921f9a746ecad61d3c84cf393
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2020-03-12 09:56:13 +0100

    Class 35. Regex II: Ejercicios
    - Exercise 1. Solved
    'Captura los emails del siguiente texto.'
    "demo@demo.com, demo_demo@demo.com.ar, demo-demo12312@sub.dom.com.ar, demo@novalido, novalido>@demo.com, demo@novalido-.com, demo@-novalido.com."
    'Resultado: ["demo@demo.com", "demo_demo@demo.com.ar", "demo-demo12312@sub.dom.com.ar"]'

--- class-35/ejercicios/ejercicio-1/js/script.js
+++ class-35/ejercicios/ejercicio-1/js/script.js
@@ -4,3 +4,28 @@
 
 // Solución aquí
 // ["demo@demo.com", "demo_demo@demo.com.ar", "demo-demo12312@sub.dom.com.ar"]
+
+
+
+const emailsString = "demo@demo.com, demo_demo@demo.com.ar, demo-demo12312@sub.dom.com.ar, demo@novalido, novalido>@demo.com, demo@novalido-.com, demo@-novalido.com";
+
+console.info(emailsString);
+
+const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+
+const newRegexEmail = new RegExp(regexEmail, "gi");
+if (newRegexEmail.test(emailsString)) {
+	console.log("Valid");
+} else {
+	console.log("Invalid");
+}
+
+
+const emailsArr = emailsString.split(", ");
+console.info(emailsArr);
+
+const emailsValid = emailsArr.filter(item => {
+	return item.match(regexEmail);
+});
+console.info(emailsValid);
