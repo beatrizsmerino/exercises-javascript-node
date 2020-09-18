584d6665a2822af252c537b59b48bcfeaa9a1e11
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2020-09-18 17:29:52 +0200

    Class 44. Firebase Auth with Email and Password
    - Added error list and verify if it is internal error
    - Refactoring code

--- class-44/clase/firebase-auth-email-password/js/script.js
+++ class-44/clase/firebase-auth-email-password/js/script.js
@@ -20,6 +20,77 @@ console.info("Firebase Conexion:", firebase);
 
 
 
+/**
+ * @const firebaseErrors
+ * @type {Object}
+ * @description List of firebase errors
+ */
+const firebaseErrors = {
+	"auth/internal-error": "Internal error",
+	"argument-error": "",
+	"app-not-authorized": "This app, identified by the domain where it's hosted, is not authorized to use Firebase Authentication with the provided API key. Review your key configuration in the Google API console.",
+	"app-not-installed": "The requested mobile application corresponding to the identifier (Android package name or iOS bundle ID) provided is not installed on this device.",
+	"captcha-check-failed": "",
+	"code-expired": "",
+	"cordova-not-ready": "Cordova framework is not ready.",
+	"cors-unsupported": "This browser is not supported.",
+	"credential-already-in-use": "This credential is already associated with a different user account.",
+	"custom-token-mismatch": "The custom token corresponds to a different audience.",
+	"requires-recent-login": "This operation is sensitive and requires recent authentication. Log in again before retrying this request.",
+	"dynamic-link-not-activated": "Please activate Dynamic Links in the Firebase Console and agree to the terms and conditions.",
+	"email-already-in-use": "The email address is already in use by another account.",
+	"expired-action-code": "The action code has expired. ",
+	"cancelled-popup-request": "This operation has been cancelled due to another conflicting popup being opened.",
+	"internal-error": "An internal error has occurred.",
+	"invalid-app-credential": "",
+	"invalid-app-id": "The mobile app identifier is not registed for the current project.",
+	"invalid-user-token": "The user's credential is no longer valid. The user must sign in again.",
+	"invalid-auth-event": "An internal error has occurred.",
+	"invalid-verification-code": "",
+	"invalid-cordova-configuration": "The following Cordova plugins must be installed to enable OAuth sign-in: cordova-plugin-buildinfo, cordova-universal-links-plugin, cordova-plugin-browsertab, cordova-plugin-inappbrowser and cordova-plugin-customurlscheme.",
+	"invalid-custom-token": "The custom token format is incorrect. Please check the documentation.",
+	"invalid-email": "The email address is badly formatted.",
+	"invalid-api-key": "Your API key is invalid, please check you have copied it correctly.",
+	"invalid-credential": "The supplied auth credential is malformed or has expired.",
+	"invalid-message-payload": "The email template corresponding to this action contains invalid characters in its message. Please fix by going to the Auth email templates section in the Firebase Console.",
+	"invalid-oauth-provider": "EmailAuthProvider is not supported for this operation. This operation only supports OAuth providers.",
+	"unauthorized-domain": "This domain is not authorized for OAuth operations for your Firebase project. Edit the list of authorized domains from the Firebase console.",
+	"invalid-action-code": "The action code is invalid. This can happen if the code is malformed, expired, or has already been used.",
+	"wrong-password": "The password is invalid or the user does not have a password.",
+	"invalid-identifier-number": "",
+	"invalid-recipient-email": "The email corresponding to this action failed to send as the provided recipient email address is invalid.",
+	"invalid-sender": "The email template corresponding to this action contains an invalid sender email or name. Please fix by going to the Auth email templates section in the Firebase Console.",
+	"invalid-verification-id": "",
+	"missing-iframe-start": "An internal error has occurred.",
+	"auth-domain-config-required": "Be sure to include authDomain when calling firebase.initializeApp(), by following the instructions in the Firebase console.",
+	"missing-app-credential": "",
+	"missing-verification-code": "",
+	"missing-identifier-number": "",
+	"missing-verification-id": "",
+	"app-deleted": "This instance of FirebaseApp has been deleted.",
+	"account-exists-with-different-credential": "An account already exists with the same email address but different sign-in credentials. Sign in using a provider associated with this email address.",
+	"network-request-failed": "A network error (such as timeout, interrupted connection or unreachable host) has occurred.",
+	"no-auth-event": "An internal error has occurred.",
+	"no-such-provider": "User was not linked to an account with the given provider.",
+	"operation-not-allowed": "The given sign-in provider is disabled for this Firebase project. Enable it in the Firebase console, under the sign-in method tab of the Auth section.",
+	"operation-not-supported-in-this-environment": 'This operation is not supported in the environment this application is running on. "location.protocol" must be http, https or chrome-extension and web storage must be enabled.',
+	"popup-blocked": "Unable to establish a connection with the popup. It may have been blocked by the browser.",
+	"popup-closed-by-user": "The popup has been closed by the user before finalizing the operation.",
+	"provider-already-linked": "User can only be linked to one identity for the given provider.",
+	"quota-exceeded": "",
+	"redirect-cancelled-by-user": "The redirect operation has been cancelled by the user before finalizing.",
+	"redirect-operation-pending": "A redirect sign-in operation is already pending.",
+	timeout: "The operation has timed out.",
+	"user-token-expired": "The user's credential is no longer valid. The user must sign in again.",
+	"too-many-requests": "We have blocked all requests from this device due to unusual activity. Try again later.",
+	"user-cancelled": "User did not grant your application the permissions it requested.",
+	"user-not-found": "There is no user record corresponding to this identifier. The user may have been deleted.",
+	"user-disabled": "The user account has been disabled by an administrator.",
+	"user-mismatch": "The supplied credentials do not correspond to the previously signed in user.",
+	"user-signed-out": "",
+	"weak-password": "The password must be 6 characters long or more.",
+	"web-storage-unsupported": "This browser is not supported or 3rd party cookies and data may be disabled."
+};
 
 
 /**
@@ -37,7 +108,6 @@ function stringToNode(string) {
 /**
  * @function labelAnimation
  * @description Add animation to label of the focus field
- * @param {Element} field
  */
 function labelAnimation() {
 	const formFields = document.getElementsByClassName("c-form__field");
@@ -448,7 +518,7 @@ function createTemplateDelete() {
 
 /**
  * @function addEventsRegister
- * @description Add events for Register form
+ * @description Add events for register form
  */
 function addEventsRegister() {
 	const buttonRegister = document.getElementById("buttonRegister");
@@ -482,7 +552,7 @@ function addEventsRegister() {
 
 /**
  * @function addEventsLogIn
- * @description Add events for Login form
+ * @description Add events for login form
  */
 function addEventsLogIn() {
 	const buttonLogIn = document.getElementById("buttonLogIn");
@@ -515,7 +585,7 @@ function addEventsLogIn() {
 
 /**
  * @function addEventsAccount
- * @description Add events for Account form
+ * @description Add events for account form
  */
 function addEventsAccount() {
 	const buttonAccountShow = document.getElementById("buttonAccountShow");
@@ -531,7 +601,7 @@ function addEventsAccount() {
 
 /**
  * @function addEventsLogOut
- * @description Add events for Logout form
+ * @description Add events for logout form
  */
 function addEventsLogOut() {
 	const buttonLogOut = document.getElementById("buttonLogOut");
@@ -547,7 +617,7 @@ function addEventsLogOut() {
 
 /**
  * @function addEventsUpdate
- * @description Add events for Update form
+ * @description Add events for update form
  */
 function addEventsUpdate() {
 	const buttonUpdateShow = document.getElementById("buttonUpdateShow");
@@ -583,7 +653,7 @@ function addEventsUpdate() {
 
 /**
  * @function addEventsDelete
- * @description Add events for Delete form
+ * @description Add events for delete form
  */
 function addEventsDelete() {
 	const buttonDeleteShow = document.getElementById("buttonDeleteShow");
@@ -612,7 +682,6 @@ function addEventsDelete() {
 
 /**
  * @function afterLogged
- * @param {Object} user User data
  * @description Function that execute after logged
  * @param {Object} user User data
  */
@@ -633,24 +702,40 @@ function afterLogged(user) {
 
 
 /**
+ * @function firebaseVerifyError
+ * @description Verify if the firebase error it is a 'Internal error' for convert it to object
+ * @param {String} text 
+ */
+function firebaseVerifyError(errorCode, errorMessage) {
+	if (errorCode === "Internal error") {
+		const stringToObject = JSON.parse(errorMessage);
+		const textMessage = stringToObject.error.message;
+		return textMessage;
+	} else {
+		return errorMessage;
+	}
+}
+
+
+/**
  * @function firebaseAuthRegisterUser
  * @description FIREBASE AUTH - REGISTER user with email and password
  * @param {String} email Email data obtained from the register form
  * @param {String} password Password data obtained from the register form
  */
-async function firebaseAuthRegisterUser(email, password) {
-	let userCredential = await firebase.auth().createUserWithEmailAndPassword(email, password)
-		.then((res) => {
+function firebaseAuthRegisterUser(email, password) {
+	firebase.auth().createUserWithEmailAndPassword(email, password)
+		.then(() => {
 			const user = firebase.auth().currentUser;
 			const userData = user.providerData;
 
-			console.group("Register!");
+			console.group("Success to register!");
 			console.info("Email: " + email, "Password: " + password);
 
 			userData.forEach(function (profile) {
 				message({
-					title: "Thanks for register!",
-					description: `${profile.email}`,
+					title: "Thanks for registering!",
+					description: profile.email,
 					className: "is-success"
 				});
 			});
@@ -660,17 +745,18 @@ async function firebaseAuthRegisterUser(email, password) {
 			// Handle Errors here.
 			const errorCode = error.code;
 			const errorMessage = error.message;
+
+			console.group("Error to register!");
 			console.warn("Error code:", errorCode);
 			console.warn("Error message:", errorMessage);
+			console.groupEnd();
 
-			if (typeof errorMessage === "string") {
-				message({
-					title: "Error for register!",
-					description: `${errorMessage}`,
-					className: "is-error"
-				});
-			}
-
+			const verifiedMessage = firebaseVerifyError(firebaseErrors[errorCode], errorMessage);
+			message({
+				title: "Error!",
+				description: verifiedMessage,
+				className: "is-error"
+			});
 		});
 }
 
@@ -681,17 +767,17 @@ async function firebaseAuthRegisterUser(email, password) {
  * @param {String} email Email data obtained from the login form
  * @param {String} password Password data obtained from the login form
  */
-async function firebaseAuthLogInUser(email, password) {
-	let userCredential = await firebase.auth().signInWithEmailAndPassword(email, password)
-		.then((res) => {
+function firebaseAuthLogInUser(email, password) {
+	firebase.auth().signInWithEmailAndPassword(email, password)
+		.then(() => {
 			const user = firebase.auth().currentUser;
 			const userData = user.providerData;
 
-			console.group("LogIn!");
+			console.group("Success login!");
 			userData.forEach(function (profile) {
 				message({
 					title: "Welcome!",
-					description: `${profile.email}`,
+					description: profile.email,
 					className: "is-success"
 				});
 			});
@@ -702,18 +788,17 @@ async function firebaseAuthLogInUser(email, password) {
 			const errorCode = error.code;
 			const errorMessage = error.message;
 
-			console.group("Error!");
+			console.group("Error to login!");
 			console.warn("Error code:", errorCode);
 			console.warn("Error message:", errorMessage);
 			console.groupEnd();
 
-			if (typeof errorMessage === "string") {
-				message({
-					title: "Error for register!",
-					description: `${errorMessage}`,
-					className: "is-error"
-				});
-			}
+			const verifiedMessage = firebaseVerifyError(firebaseErrors[errorCode], errorMessage);
+			message({
+				title: "Error!",
+				description: verifiedMessage,
+				className: "is-error"
+			});
 		});
 }
 
@@ -738,11 +823,14 @@ function firebaseAuthUpdateUser(name, photo) {
 			const user = firebase.auth().currentUser;
 			const userData = user.providerData;
 
-			console.group("Updated!");
+			console.group("Success user account updated!");
 			userData.forEach(function (profile) {
+				console.info("Provider-specific UID:", profile.uid);
+				console.info("Email:", profile.email);
+
 				message({
 					title: "Account updated!",
-					description: `Provider-specific UID: profile.uid<br> Name: ${profile.displayName}<br> Email: ${profile.email}<br> Photo: ${profile.photoURL}`,
+					description: `Name: ${profile.displayName}<br> Photo: ${profile.photoURL}`,
 					className: "is-success"
 				});
 			});
@@ -754,16 +842,18 @@ function firebaseAuthUpdateUser(name, photo) {
 			// Handle Errors here.
 			const errorCode = error.code;
 			const errorMessage = error.message;
+
+			console.group("Error user account updated!");
 			console.warn("Error code:", errorCode);
 			console.warn("Error message:", errorMessage);
+			console.groupEnd();
 
-			if (typeof errorMessage === "string") {
-				message({
-					title: "Error for register!",
-					description: `${errorMessage}`,
-					className: "is-error"
-				});
-			}
+			const verifiedMessage = firebaseVerifyError(firebaseErrors[errorCode], errorMessage);
+			message({
+				title: "Error!",
+				description: verifiedMessage,
+				className: "is-error"
+			});
 		});
 }
 
@@ -784,11 +874,11 @@ function firebaseAuthDeleteUser(password) {
 
 	user.reauthenticateWithCredential(credential)
 		.then(function () {
-			console.group("Reauthenticate!");
+			console.group("Reauthenticate user account!");
 			userData.forEach(function (profile) {
 				message({
 					title: "Reauthenticate user account!",
-					description: `${profile.email}`,
+					description: profile.email,
 					className: "is-success"
 				});
 			});
@@ -796,11 +886,11 @@ function firebaseAuthDeleteUser(password) {
 
 			user.delete()
 				.then(function () {
-					console.group("Deleted!");
+					console.group("Deleted user account!");
 					userData.forEach(function (profile) {
 						message({
 							title: "Account deleted :(",
-							description: `${profile.email}`,
+							description: profile.email,
 							className: "is-success"
 						});
 					});
@@ -811,36 +901,36 @@ function firebaseAuthDeleteUser(password) {
 					const errorCode = error.code;
 					const errorMessage = error.message;
 
-					console.group("Fail delete user!");
+					console.group("Error to delete user!");
 					console.warn("Error code:", errorCode);
 					console.warn("Error message:", errorMessage);
 					console.groupEnd();
 
-					if (typeof errorMessage === "string") {
-						message({
-							title: "Error for register!",
-							description: `${errorMessage}`,
-							className: "is-error"
-						});
-					}
+					const verifiedMessage = firebaseVerifyError(firebaseErrors[errorCode], errorMessage);
+					message({
+						title: "Error!",
+						description: verifiedMessage,
+						className: "is-error"
+					});
+
 				});
 		})
 		.catch(function (error) {
 			const errorCode = error.code;
 			const errorMessage = error.message;
 
-			console.group("Fail reauthenticate!");
+			console.group("Error to user reauthenticate!");
 			console.warn("Error code:", errorCode);
 			console.warn("Error message:", errorMessage);
 			console.groupEnd();
 
-			if (typeof errorMessage === "string") {
-				message({
-					title: "Error for register!",
-					description: `${errorMessage}`,
-					className: "is-error"
-				});
-			}
+			const verifiedMessage = firebaseVerifyError(firebaseErrors[errorCode], errorMessage);
+			message({
+				title: "Error!",
+				description: verifiedMessage,
+				className: "is-error"
+			});
+
 		});
 }
 
@@ -848,18 +938,19 @@ function firebaseAuthDeleteUser(password) {
 /**
  * @function firebaseAuthLogOutUser
  * @description FIREBASE AUTH - LOGOUT user with email and password
- * @param {String} email Email data obtained from the logout form
- * @param {String} password Password data obtained from the logout form
  */
-async function firebaseAuthLogOutUser() {
+function firebaseAuthLogOutUser() {
 	const userEmail = firebase.auth().currentUser.email;
-	console.info("Saliendo del sistema:", userEmail);
 
-	let userCredential = await firebase.auth().signOut()
-		.then((res) => {
+	firebase.auth().signOut()
+		.then(() => {
+			console.group("Success to logout!");
+			console.info("Exiting the system:", userEmail);
+			console.groupEnd();
+
 			message({
 				title: "Good bye!",
-				description: `${userEmail}`,
+				description: userEmail,
 				className: "is-success"
 			});
 		})
@@ -868,18 +959,17 @@ async function firebaseAuthLogOutUser() {
 			const errorCode = error.code;
 			const errorMessage = error.message;
 
-			console.group("Logged out error!");
+			console.group("Error!");
 			console.warn("Error code:", errorCode);
 			console.warn("Error message:", errorMessage);
 			console.groupEnd();
 
-			if (typeof errorMessage === "string") {
-				message({
-					title: "Error for register!",
-					description: `${errorMessage}`,
-					className: "is-error"
-				});
-			}
+			const verifiedMessage = firebaseVerifyError(firebaseErrors[errorCode], errorMessage);
+			message({
+				title: "Error!",
+				description: verifiedMessage,
+				className: "is-error"
+			});
 		});
 }
 
